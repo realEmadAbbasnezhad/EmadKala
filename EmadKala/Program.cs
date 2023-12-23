a264ca143a647a392ac44a70eaa1e9fa185b4952
real_emad_abbasnezhad <real_emad_abbasnezhad@proton.me>
2023-12-23 13:19:49 +0330

    add data base and identity

--- EmadKala/Program.cs
+++ EmadKala/Program.cs
@@ -5,24 +5,38 @@
  * You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License. */
 
+using EmadKala.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-#region HTTP request pipeline
+#region Services
+
+// data base (MySQL, EF core)
+var connectionString = builder.Configuration.GetConnectionString("docker-mysql");
+
+builder.Services.AddDbContext<EmadKalaDbContext>(opt => opt.UseMySql(
+    connectionString, ServerVersion.AutoDetect(connectionString)));
+
+// identity (IdentityCore)
+builder.Services.AddIdentityCore<IdentityUser>().AddEntityFrameworkStores<EmadKalaDbContext>();
 
 #endregion
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 #region HTTP request pipeline
+
+// make sure data base is created.
+app.Services.CreateScope().ServiceProvider.GetService<EmadKalaDbContext>()?.Database.EnsureCreated();
 
 #endregion
 
