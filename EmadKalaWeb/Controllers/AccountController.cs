514606e6bd3921b822c07282d09a3747e587095c
real_emad_abbasnezhad <real_emad_abbasnezhad@proton.me>
2023-12-25 13:38:30 +0330

    add API and account controller

---
+++ EmadKalaWeb/Controllers/AccountController.cs
@@ -0,0 +1,14 @@
+// Copyright (C) 2023-2024 Emad Abbasnezhad (real_emad_abbasnezhad@proton.me)
+// This file is a part of EmadKala, Licenced under http://www.gnu.org/licenses/
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmadKalaWeb.Controllers;
+
+/// <summary>
+/// sing in, sing up and everything related to users account is handling by this controller.
+/// </summary>
+[ApiController, Route("account")]
+public class AccountController : Controller
+{
+}
--- EmadKalaWeb/Program.cs
+++ EmadKalaWeb/Program.cs
@@ -1,13 +1,22 @@
 // Copyright (C) 2023-2024 Emad Abbasnezhad (real_emad_abbasnezhad@proton.me)
 // This file is a part of EmadKala, Licenced under http://www.gnu.org/licenses/
 
+using System.Reflection;
 using EmadKalaWeb.Models;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 #region Services
+
+// core (ASP net core MVC)
+builder.Services.AddControllersWithViews();
+
+// api (Swagger)
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(opt =>
+    opt.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory,
+        $"{Assembly.GetExecutingAssembly().GetName().Name}.xml")));
 
 // data base (MySQL, EF core)
 var connectionString = builder.Configuration.GetConnectionString("docker-mysql");
@@ -24,8 +33,15 @@ var app = builder.Build();
 
 #region HTTP request pipeline
 
-// make sure data base is created.
-app.Services.CreateScope().ServiceProvider.GetService<EmadKalaDbContext>()?.Database.EnsureCreated();
+if (app.Environment.IsDevelopment())
+{
+    // add swagger
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
+    // make sure data base is created.
+    app.Services.CreateScope().ServiceProvider.GetService<EmadKalaDbContext>()?.Database.EnsureCreated();
+}
 
 #endregion
 
