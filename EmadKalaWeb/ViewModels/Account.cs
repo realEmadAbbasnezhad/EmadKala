de825eee48f22762632433d67ed742063d6af4fc
real_emad_abbasnezhad <real_emad_abbasnezhad@proton.me>
2023-12-26 10:08:14 +0330

    update Account view models

--- EmadKalaWeb/ViewModels/Account.cs
+++ EmadKalaWeb/ViewModels/Account.cs
@@ -1,13 +1,13 @@
+// This file is a part of EmadKala, Licenced under https://www.gnu.org/licenses/gpl-3.0.html
 // Copyright (C) 2023-2024 Emad Abbasnezhad (real_emad_abbasnezhad@proton.me)
-// This file is a part of EmadKala, Licenced under http://www.gnu.org/licenses/
 
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace EmadKalaWeb.ViewModels;
-
 #pragma warning disable CS8618
-//TODO: Hare you can change texts shows in the form
+// [Quick Start] model views are a good place to start localization.
+
 public class SignupViewModel
 {
     [DisplayName("شماره همراه"), DataType(DataType.PhoneNumber)]
@@ -19,4 +19,48 @@ public class SignupViewModel
     [Required(ErrorMessage = "رمز عبور وارد نشده")]
     public string Password { get; set; }
 }
+
+public class SigninPasswordViewModel
+{
+    [DisplayName("شماره همراه"), DataType(DataType.PhoneNumber)]
+    [Required(ErrorMessage = "شماره همراه وارد نشده")
+     , RegularExpression(@"^(?:0)?(9\d{9})$", ErrorMessage = "شماره همراه معتبر نیست")]
+    public string PhoneNumber { get; set; }
+
+    [DisplayName("رمز عبور"), DataType(DataType.Password)]
+    [Required(ErrorMessage = "رمز عبور وارد نشده")]
+    public string Password { get; set; }
+}
+
+public class PhoneNumberSigninViewModel
+{
+    [DisplayName("شماره همراه"), DataType(DataType.PhoneNumber)]
+    [Required(ErrorMessage = "شماره همراه وارد نشده")
+     , RegularExpression(@"^(?:0)?(9\d{9})$", ErrorMessage = "شماره همراه معتبر نیست")]
+    public string PhoneNumber { get; set; }
+}
+
+public class PhoneNumberConfirmationCodeSigninViewModel
+{
+    [DisplayName("کد یک بار مصرف"), DataType(DataType.Text)]
+    [Required(ErrorMessage = "کد یک بار وارد نشده")]
+    public string Code { get; set; }
+}
+
+public class PhoneNumberConfirmationViewModel
+{
+    [DisplayName("شماره همراه"), DataType(DataType.PhoneNumber)]
+    [Required(ErrorMessage = "شماره همراه وارد نشده")
+     , RegularExpression(@"^(?:0)?(9\d{9})$", ErrorMessage = "شماره همراه معتبر نیست")]
+    public string PhoneNumber { get; set; }
+}
+
+public class PhoneNumberConfirmationCodeViewModel
+{
+    [DisplayName("کد یک بار مصرف"), DataType(DataType.Text)]
+    [Required(ErrorMessage = "کد یک بار وارد نشده")]
+    public string Code { get; set; }
+}
+
+
 #pragma warning restore CS8618
